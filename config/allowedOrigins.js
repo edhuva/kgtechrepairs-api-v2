3552926c186ce07e94f48554c08d19d0e6af05a1
edhuva <edhuva491@gmail.com>
2024-06-14 21:26:11 +0200

    updating - code ready to deploy backend

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,5 @@
 const allowedOrigins = [
-    'http://localhost:3000'
-    // 'http://localhost:3000',
-    // 'https://www.dandrepairshop.com',
-    // 'https://dandrepairshop.com'
+    'https://kgtechrepairs-v2.onrender.com/',
 ]
 
 module.exports = allowedOrigins
